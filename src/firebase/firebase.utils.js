415de8f039c952a73974c21ff17f4c52f7f79bd9
SARUGAN LOGAN <saruganlogan@hotmail.com>
2020-05-08 11:11:02 -0400

    updated Firebase Utils and stripe-button w/ My creds

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+    apiKey: "AIzaSyAsw7aZHMmXummKG9XJHXpk_8OrxLNbwu4",
+    authDomain: "crwn-db-c9005.firebaseapp.com",
+    databaseURL: "https://crwn-db-c9005.firebaseio.com",
+    projectId: "crwn-db-c9005",
+    storageBucket: "crwn-db-c9005.appspot.com",
+    messagingSenderId: "7761442491",
+    appId: "1:7761442491:web:09ed2c1b519f25e1bb1b9e",
+    measurementId: "G-PKCKXJHC1W"
 };
 
 firebase.initializeApp(config);
